3c505169818088b107e36e5d7082c13dcdb25d15
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-19 10:08:01 +0400

    Update CoursesEntity.java
    add course entity

--- src/main/java/com/example/ingresspaymentproject/entity/CoursesEntity.java
+++ src/main/java/com/example/ingresspaymentproject/entity/CoursesEntity.java
@@ -1,4 +1,13 @@
 package com.example.ingresspaymentproject.entity;
 
+@Entity
+@Table(name = "courses")
 public class CoursesEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+  @Column(name = "course_name")
+  private String courseName;
+  @Column(name = "is_active")
+  private boolean isActive;
 }
