74b26236693be0e52423a2a26a056a42b57794f2
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-20 11:29:43 +0400

    Update PaymentEntity.java
    add payment entity

--- src/main/java/com/example/ingresspaymentproject/entity/PaymentEntity.java
+++ src/main/java/com/example/ingresspaymentproject/entity/PaymentEntity.java
@@ -1,4 +1,22 @@
 package com.example.ingresspaymentproject.entity;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name = "payments")
 public class PaymentEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  private String courseName;
+  private int courseMonth
+  private String studentName;
+  private int amount;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private Date receiptDate;
+  @Lob
+  private byte[] receiptImage;
 }
