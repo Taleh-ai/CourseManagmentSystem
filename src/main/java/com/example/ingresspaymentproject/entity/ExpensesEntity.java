3f2995b8c2cc4c21954e3c75b009ea530a7e7a49
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-20 11:29:33 +0400

    Update ExpensesEntity.java
    add expenses

--- src/main/java/com/example/ingresspaymentproject/entity/ExpensesEntity.java
+++ src/main/java/com/example/ingresspaymentproject/entity/ExpensesEntity.java
@@ -1,4 +1,19 @@
 package com.example.ingresspaymentproject.entity;
-
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+  @Table(name = "expenses")
 public class ExpensesEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+  private String expensesName;
+  private String description;
+  private int amount;
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private Date expensesDate;
+  @Lob
+  private byte[] expensesImage;
 }
